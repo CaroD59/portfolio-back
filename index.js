ffeecc92427c217f2ef113aa78d5c43797a6a423
CaroD59 <dorchies.c@gmail.com>
2021-07-19 09:30:09 +0200

    Save final form

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const { backPort } = require('./db-config');
 const app = express();
 require('dotenv').config();
 const gitRoutes = require('./Routes/git');
+const contactRoutes = require('./Routes/contactform');
 
 app.use(express.json());
 app.use(cors());
 app.use('/creations', gitRoutes);
+app.use('/contact', contactRoutes);
 
 app.listen(backPort, () => {
   console.log(`🚀 Server running on http://localhost:${backPort}/`);
